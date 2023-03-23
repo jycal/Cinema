a2fb7b02543d708390160423e8851f7b71b72e8c
meganpieters <meganpieters3@gmail.com>
2023-03-23 16:02:31 +0100

    film

---
+++ Cinema/DataAccess/FilmsAcces.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using System.Text.Json;
+
+static class FilmsAccess
+{
+    static string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, @"DataSources/films.json"));
+
+
+    public static List<FilmModel> LoadAll()
+    {
+        string json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<List<FilmModel>>(json);
+    }
+
+
+    public static void WriteAll(List<FilmModel> films)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        string json = JsonSerializer.Serialize(films, options);
+        File.WriteAllText(path, json);
+    }
+
+
+
+}
