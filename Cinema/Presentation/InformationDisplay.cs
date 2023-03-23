d5ac69a2bce03a955f8b22294176b7b8422cfff1
JulinaMay <julinamay_@Hotmail.com>
2023-03-23 16:00:40 +0100

    Information display

---
+++ Cinema/Presentation/InformationDisplay.cs
@@ -0,0 +1,13 @@
+public class InformationDisplay
+{
+    string overview = @"
+============================================
+|                CINEMA INFO               |
+============================================
+| Phone number:  +31-655-574-244.          |
+| Location:      Wijnhaven 107,            |
+|                3011 WN  in Rotterdam     |
+| Email:         StarlightCinema@gmail.com |
+============================================
+";
+}
