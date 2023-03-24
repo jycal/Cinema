00876e89dcd28ff950a10c4e006af570fde244d7
meganpieters <meganpieters3@gmail.com>
2023-03-24 11:00:33 +0100

    films

--- Cinema/DataModels/FilmModel.cs
+++ Cinema/DataModels/FilmModel.cs
@@ -1,10 +1,33 @@
+using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-public FilmModel(int id, string title, string description, int duration, List<string> genre)
+
+class FilmModel
 {
-    Id = id;
-    Title = title;
-    Description = description;
-    Duration = duration;
-    Genre = genre;
-}
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("duration")]
+    public int Duration { get; set; }
+
+    [JsonPropertyName("genre")]
+    public List<string> Genre { get; set; }
+
+    public FilmModel(int id, string title, string description, int duration, List<string> genre)
+    {
+        Id = id;
+        Title = title;
+        Description = description;
+        Duration = duration;
+        Genre = genre;
+    }
 }
