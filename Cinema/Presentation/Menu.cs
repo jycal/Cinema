ffcb2661e6d7fb29a244f6aaaec460fa2bca267f
JulinaMay <julinamay_@Hotmail.com>
2023-03-24 13:06:03 +0100

    Menu / Userlogin geupdate
    als je inlogt krijg je alleen bepaalde opties te zien in het menu :D

--- Cinema/Presentation/Menu.cs
+++ Cinema/Presentation/Menu.cs
@@ -1,38 +1,90 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 static class Menu
 {
+    private static bool isLoggedIn = false;
 
-    //This shows the menu. You can call back to this method to show the menu again
-    //after another presentation method is completed.
-    //You could edit this to show different menus depending on the user's role
     static public void Start()
     {
-        Console.WriteLine("Enter 1 to login");
-        Console.WriteLine("Enter 2 to view cinema info");
-        Console.WriteLine("Enter 3 to add/delete film");
-
-        string input = Console.ReadLine();
-        if (input == "1")
+        Console.WriteLine();
+        Console.WriteLine("====================================");
+        Console.WriteLine("|                                  |");
+        Console.WriteLine("|            Main Menu             |");
+        Console.WriteLine("|                                  |");
+        Console.WriteLine("====================================");
+        Console.WriteLine();
+        Console.WriteLine("Please select an option:");
+        if (!isLoggedIn)
         {
-            UserLogin.Start();
-        }
-        else if (input == "2")
-        {
-            Console.WriteLine(InformationDisplay.overview);
-        }
-        else if (input == "3")
-        {
-            FilmManagerMenu.MovieOverview();
-            FilmManagerMenu.Start();
+            Console.WriteLine("1. Login as member");
+            Console.WriteLine("2. Login as employee");
+            Console.WriteLine("3. Login as admin");
+            Console.WriteLine("4. View movie info");
+            Console.WriteLine("5. View cinema info");
         }
         else
         {
-            Console.WriteLine("Invalid input");
-            Start();
+            Console.WriteLine("1. View movie info");
+            Console.WriteLine("2. Add/delete film\n");
+            Console.WriteLine("3. View cinema info");
+            Console.WriteLine("4. Logout");
+            Console.Write("Enter your choice: ");
         }
 
+        string input = Console.ReadLine();
+
+        if (!isLoggedIn)
+        {
+            if (input == "1")
+            {
+                UserLogin.Start(ref isLoggedIn);
+            }
+            else if (input == "2")
+            {
+                UserLogin.Start(ref isLoggedIn);
+            }
+            else if (input == "3")
+            {
+                UserLogin.Start(ref isLoggedIn);
+            }
+            else if (input == "4")
+            {
+                // Hier moet de connecttie komen naar de movieoverview
+                // FilmManagerMenu.MovieOverview();
+            }
+            else if (input == "5")
+            {
+                Console.WriteLine(InformationDisplay.overview);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                Start();
+            }
+        }
+        else
+        {
+            if (input == "1")
+            {
+                // Hier moet de connecttie komen naar de movieoverview
+                // FilmManagerMenu.MovieOverview();
+            }
+            else if (input == "2")
+            {
+                FilmManagerMenu.Start();
+            }
+            else if (input == "3")
+            {
+                Console.WriteLine(InformationDisplay.overview);
+            }
+            else if (input == "4")
+            {
+                isLoggedIn = false;
+                Console.WriteLine("You have been logged out.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                Start();
+            }
+        }
     }
 }
