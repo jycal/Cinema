80e7a8c6174a8c04c57baff085883a3c5896d3ad
meganpieters <meganpieters3@gmail.com>
2023-06-08 18:46:18 +0200

    seat selection fixed

--- Cinema/Presentation/Payment.cs
+++ Cinema/Presentation/Payment.cs
@@ -6,9 +6,9 @@ public static class Payment
         bool isValid = false;
         do
         {
-            Console.WriteLine("\n--------------------------------");
-            Console.WriteLine("         PAYPAL PAYMENT         ");
-            Console.WriteLine("--------------------------------");
+            Console.WriteLine("\n--------------------------------".BrightCyan());
+            Console.WriteLine("         PAYPAL PAYMENT         ".BrightWhite());
+            Console.WriteLine("--------------------------------".BrightCyan());
             Console.WriteLine("To go through with the payment, please enter 4 digits of your bank number:");
             Console.WriteLine("To return to the main menu, enter '0'.");
             bank = Console.ReadLine()!;
@@ -49,9 +49,9 @@ public static class Payment
         bool isValid = false;
         do
         {
-            Console.WriteLine("\n--------------------------------");
-            Console.WriteLine("          IDEAL PAYMENT         ");
-            Console.WriteLine("--------------------------------");
+            Console.WriteLine("\n--------------------------------".BrightCyan());
+            Console.WriteLine("          IDEAL PAYMENT         ".BrightWhite());
+            Console.WriteLine("--------------------------------".BrightCyan());
             Console.WriteLine("Please enter your bank number (format: NL00):\n");
             bank = Console.ReadLine()!;
 
